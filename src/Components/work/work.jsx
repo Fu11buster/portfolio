f99fd713f55635fc5606b47f6589e731aaaa35ad
Nikolay Stepin <bukato66@gmail.com>
2019-05-14 16:34:42 +0800

    added works page

--- src/Components/work/work.jsx
+++ src/Components/work/work.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 import './work.css';
 
 const Work = ({title, description, link, bgImage, logo}) => (
-  <article className="work">
-
-  </article>
+  <li>
+    <article className="work" style={{backgroundImage: `url(${bgImage})`}}>
+      <a href={link} className="work__link">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <img src={logo} alt={`${title} logo`} className="work__logo" />
+      </a>
+    </article>
+  </li>
 )
 
 export default Work;
--- src/Components/worksPage/worksPage.jsx
+++ src/Components/worksPage/worksPage.jsx
@@ -3,27 +3,36 @@ import Work from '../work/work';
 
 import './worksPage.css';
 
+import dobrotaBG from '../../img/dobrota-bg.jpg';
+import dobrotaLogo from '../../img/dobrota.svg'
+import alcorBG from '../../img/alcor-bg.jpg';
+import alcorLogo from '../../img/alcor.svg';
+
 const WORKS = [
   {
     title: 'Dobrota',
-    description: '',
+    description: 'This is a promotional website created for the Baikal Film Festival.',
     link: 'http://mydobrota.ru/',
-    bgImage: '',
-    logo: '',
+    bgImage: dobrotaBG,
+    logo: dobrotaLogo,
   },
   {
     title: 'Alcor',
-    description: '',
+    description: 'Landing page for steel processing companies.',
     link: 'http://alcor-bg.com/',
-    bgImage: '',
-    logo: '',
+    bgImage: alcorBG,
+    logo: alcorLogo,
   }
 ]
 
 const WorksPage = () => (
   <section className="works">
     <h2>My latest works:</h2>
-    {WORKS.map()}
+    <ul className="latest-works">
+      {WORKS.map(work => (
+        <Work {...work} />
+      ))}
+    </ul>
   </section>
 )
 
